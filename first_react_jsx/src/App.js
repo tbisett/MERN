2cb1f880c37162f016f1f407f099197549eb0439
Taylor Bisett <taylorbisett@gmail.com>
2021-09-01 11:50:25 -0700

    iteration 1

---
+++ first_react_jsx/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="fisrtJSX">
+      <h1>Hello Dojo</h1>
+      <h2>Things I need to do:</h2>
+      <ul>
+        <li>Learn React</li>
+        <li>Run a marathon</li>
+        <li>Climb Mount Everest</li>
+        <li>Feed the dogs</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
